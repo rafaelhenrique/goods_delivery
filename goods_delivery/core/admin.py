c6e0b2135434029d41bccdc245a8a2f259551e3f
Rafael Henrique da Silva Correia <rafael@abraseucodigo.com.br>
2016-12-17 19:11:24 -0200

    Add admin MapAdmin and RouteAdmin

--- goods_delivery/core/admin.py
+++ goods_delivery/core/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Map, Route
 
-# Register your models here.
+
+class MapAdmin(admin.ModelAdmin):
+    list_display = ('name', 'get_routes')
+
+    def get_routes(self, obj):
+        routes = [str(route) for route in obj.routes.all()]
+        return "</br>".join(routes)
+
+    get_routes.allow_tags = True
+    get_routes.short_description = 'Rotas'
+
+
+class RouteAdmin(admin.ModelAdmin):
+    list_display = ('start', 'end', 'distance')
+
+admin.site.register(Map, MapAdmin)
+admin.site.register(Route, RouteAdmin)
